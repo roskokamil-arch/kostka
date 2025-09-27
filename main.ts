a465d6dc49f942c69396a70083f0da69702b51fb
roskokamil-arch <rosko.kamil@gmail.com>
2025-09-27 16:46:08 +0200

    Aktualizovat main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    RingbitCar.forward()
+radio.onReceivedNumber(function (receivedNumber) {
+    a = receivedNumber
 })
-input.onButtonPressed(Button.B, function () {
-    RingbitCar.back()
-})
+let a = 0
+radio.setGroup(1)
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+a = 0
+basic.forever(function () {
+    if (a == 1) {
+        RingbitCar.forward()
+        basic.pause(500)
+        RingbitCar.turnright()
+        basic.pause(100)
+    } else {
+        RingbitCar.brake()
+    }
+})
